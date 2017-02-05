dfd69718f24be2b64070b89b577de94654814d5c
Andrew Dirksen <andrew@dirksen.com>
2017-02-05 13:41:07 -0800

    autoscale veiw

--- index.js
+++ index.js
@@ -1,9 +1,41 @@
 const can = document.getElementById('canvas')
 const ctx = can.getContext('2d')
 
-let [x, y] = [can.width / 2, can.height / 2]
+can.width = innerWidth
+can.height = innerHeight
+
+const start = [0, 0]
+
+const path = [start]
+
+let [xymin, xymax] = [0, 0]
 
 setInterval(() => {
-  [x, y] = [Math.random() * 2 - 1 + x, Math.random() * 2 - 1 + y]
-  ctx.fillRect(x - 1, y - 1, 2, 2);
-}, 10)
+  const [last_x, last_y] = path[path.length - 1]
+
+  const [x, y] = [
+    Math.random() * 2 - 1 + last_x,
+    Math.random() * 2 - 1 + last_y
+  ]
+
+  path.push([x, y])
+
+  xymin = Math.min(xymin, x)
+  xymax = Math.max(xymax, x)
+  xymin = Math.min(xymin, y)
+  xymax = Math.max(xymax, y)
+
+  ctx.clearRect(0, 0, can.width, can.height)
+  ctx.beginPath()
+  ctx.moveTo(
+    (start[0] - xymin) / (xymax - xymin) * can.width,
+    (start[1] - xymin) / (xymax - xymin) * can.height
+  )
+  for (const [x, y] of path) {
+    ctx.lineTo(
+      (x - xymin) / (xymax - xymin) * can.width,
+      (y - xymin) / (xymax - xymin) * can.height
+    )
+  }
+  ctx.stroke()
+}, 0)
